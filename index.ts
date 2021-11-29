d50fd6b17d007928f05b6cf882e9f1e37369bbfd
Salvo Bonanno <salvob.labs@gmail.com>
2021-11-29 16:28:53 +0100

    Declare classes

---
+++ index.ts
@@ -0,0 +1,19 @@
+// Transfer of funds between two wallets
+class Transaction {
+  // ...
+}
+
+// Individual block on the chain
+class Block {
+  // ...
+}
+
+// The blockchain
+class Chain {
+  // ...
+}
+
+// Wallet gives a user a public/private keypair
+class Wallet {
+  // ...
+}
