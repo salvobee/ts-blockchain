6c719838607f0325f2b1c637bdf0c54d56508d69
Salvo Bonanno <salvob.labs@gmail.com>
2021-11-29 16:37:49 +0100

    Ignites the blockchain creating some wallets and adding some transactions.

--- index.ts
+++ index.ts
@@ -101,3 +101,15 @@ class Wallet {
     Chain.instance.addBlock(transaction, this.publicKey, signature);
   }
 }
+
+
+
+const satoshi = new Wallet();
+const bob = new Wallet();
+const alice = new Wallet();
+
+satoshi.sendMoney(50, bob.publicKey);
+bob.sendMoney(23, alice.publicKey);
+alice.sendMoney(5, bob.publicKey);
+
+console.log(Chain.instance);
